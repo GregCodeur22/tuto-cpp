7775403ae3da2bf882016104e7293bdded5719eb
gregcode <gregcode22@gmail.com>
2025-08-18 19:22:00 +0200

    debut character

---
+++ cpp04/ex03/Character.cpp
@@ -0,0 +1,37 @@
+#include "Character.hpp"
+#include <iostream>
+
+Character::Character(std::string const &name) : _name(name)
+{
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = 0;
+}
+
+Character::~Character()
+{
+	for (int i = 0; i < 4; i++)
+		delete _inventory[i];
+}
+
+std::string const & Character::getName() const
+{
+	return _name;
+}
+
+void Character::equip(AMateria *m)
+{
+	for(int i = 0; i < 4; i++)
+	{
+		if (_inventory[i] == 0)
+		{
+			_inventory[i] = m;
+			return ;
+		}
+	}
+}
+
+void Character::unequip(int idx)
+{
+	if (idx >= 0 && idx < 4)
+		_inventory[idx] = 0;
+}
---
+++ cpp04/ex03/Character.hpp
@@ -0,0 +1,24 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include "ICharacter.hpp"
+#include "AMateria.hpp"
+#include <string>
+
+class Character : public ICharacter
+{
+	private:
+		std::string _name;
+		AMateria* _inventory[4];
+
+	public:
+		Character(std::string const &name);
+		virtual ~Character();
+		
+		std::string const &getName() const;
+		void equip(AMateria *m);
+		void unequip(int idx);
+		void use(int idx, ICharacter &target);
+};
+
+#endif
